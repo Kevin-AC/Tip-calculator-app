3668af994e46f005130b3a952a9ddb8a460e42a6
Kevin-AC <kevin.argumedo@gmail.com>
2023-01-09 19:16:28 -0500

    terminado

--- main.js
+++ main.js
@@ -1,82 +1,91 @@
 
 const bill = document.getElementById('Bill');
+let billValue=bill.value
 const people=document.getElementById('People')
+let peopleValue=people.value
+const peopleSpam=document.getElementById('errorTx')
 
-const botones=document.getElementById('botones')
+//const botones=document.getElementById('botones')
+const botones=document.querySelectorAll('.butons')
 const btn5 = document.getElementById('btn5')
 const btn10 = document.getElementById('btn10')
 const btn15 = document.getElementById('btn15')
 const btn25 = document.getElementById('btn25')
 const btn50=document.getElementById('btn50')
 const custom=document.getElementById('Custom')
+let customValue=custom.value
 const reset=document.getElementById('Reset')
 
 const tipamount=document.getElementById('tipAmount')
 const total=document.getElementById('Total')
 
-let porcentaje
-let factura
-let Npeople
+let valorTip=0
 
-if(bill.value==""&&people.value==""){
-    factura=0
-    Npeople=0
-}
-
-custom.addEventListener('input',e=>{
-    porcentaje=custom.value/100
+botones.forEach(element=>{
+    element.addEventListener('click',e=>{
+        restablecer()
+        element.classList.add('butonsActive')
+        valorTip=parseInt(e.target.innerText.slice(0,-1))
+        calcularTip()
+       console.log(peopleValue)
+    })
 })
 
-botones.addEventListener("click",e=>{
-    if(e.target.innerText=="5%"){
-        porcentaje=0.05
-    }else if(e.target.innerText=="10%"){
-        porcentaje=0.10
-    }else if(e.target.innerText=="15%"){
-        porcentaje=0.15
-    }else if(e.target.innerText=="25%"){
-        porcentaje=0.25
-    }else if(e.target.innerText=="50%"){
-        porcentaje=0.50
-    }else if(e.target.innerText=="5%\n10%\n15%\n25%\n50%"){
-        porcentaje=0
-    }  
+bill.addEventListener('input',()=>{
+    billValue=bill.value
+    calcularTip()
    
 })
+people.addEventListener('input',()=>{
+    peopleValue=people.value
+    console.log(peopleValue)
+    if(peopleValue==0){
+        people.classList.add('error')
+        peopleSpam.innerText=`Can't be zero`
+    }else{
+        people.classList.remove('error')
+        peopleSpam.innerText=`` 
+        calcularTip()
+    }
+    
+   
+})
+custom.addEventListener('input',()=>{
+    valorTip=parseInt(custom.value)
+    if(isNaN(valorTip)){
 
-people.addEventListener('input', e => {
-    let tip
-    let facturaP
-    factura = bill.value
-    Npeople = people.value
-    tip = factura * porcentaje
-    tip = (tip / Npeople).toFixed(2)
-    facturaP=(factura/Npeople).toFixed(2)
-    tipamount.innerHTML = `<p class="text-3xl text-Strongcyan">$${tip}</p>`
-    total.innerHTML=`<p class="text-3xl text-Strongcyan">$${facturaP}</p>`
+    }else{
+        calcularTip()
+        restablecer()
+    }
+
+    
 })
 
 reset.addEventListener('click',e=>{
-    bill.value=""
-    people.value=""
-    custom.value=""
-    tipamount.innerHTML = `<p class="text-3xl text-Strongcyan">$0.00</p>`
-    total.innerHTML=`<p class="text-3xl text-Strongcyan">$0.00</p>`
+    people.classList.remove('error')
+    peopleSpam.innerText=`` 
+    bill.value=``
+    people.value=``
+    custom.value=``
+    tipamount.innerText=`$0.00`
+    total.innerText=`$0.00`
+    restablecer()
 })
 
-
-// function calculoP(porcentaje){
-//     let factura
-//     let Npeople
-//     let total=0
-//     
-//     factura=bill.value
-//     Npeople=people.value
-//     total=factura*porcentaje
-//     total=(total/Npeople).toFixed(2)
-//     console.log(total)
-//     tipamount.innerHTML=`<p class="text-3xl text-Strongcyan">$${total}</p>`
-
-// }
-
-
+function calcularTip(){
+    if(tipamount.innerText==NaN){
+        tipamount.innerText =`0.00`
+    }
+    let calculotip = ((billValue * valorTip / 100) / people.value)
+    tipamount.innerText = (calculotip).toFixed(2)
+    let totalP = ((billValue / people.value) + calculotip)
+    total.innerText = (totalP).toFixed(2)
+    
+}
+function restablecer(){
+    
+    botones.forEach(element=>{
+        element.classList.remove('butonsActive')
+    })
+}
